1b4332c5d4ca150a7c961a8031248e5a501c2a6a
internabhijit <[abhijitdeshmukh10@gmail.com]>
2019-06-05 09:35:34 +0530

    added GET /todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {User} = require('./models/user');
@@ -27,6 +28,20 @@ app.get('/todos', (req, res) => {
     Todo.find()
     .then((todos) => res.send({todos}))
     .catch((e) => res.status(400).send(e))
+})
+
+// GET todos/12345
+app.get('/todos/:id', (req, res) => {
+
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) return res.status(400).send()
+
+    Todo.findById(id)
+    .then((todo) => {
+        if (!todo) return res.status(404).send()
+        res.send({todo});
+    })
+    .catch((e) => res.status(400).send())
 })
 
 app.listen(3000, () => {
